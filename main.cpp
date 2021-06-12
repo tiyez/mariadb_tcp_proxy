62f58ec43d911fc174f73f933b2a7168edf3ffc4
Joline Sandslash <jsandsla@un-h2.kzn.21-school.ru>
2021-06-12 18:47:09 +0300

    args

--- main.cpp
+++ main.cpp
@@ -9,12 +9,21 @@
 
 using boost::asio::ip::tcp;
 
-int		main() {
-	try {
-		boost::asio::io_context	io;
-		tcp_proxy	proxy(io, "mariadb", "3306", tcp::endpoint(tcp::v4(), 3306), boost::make_shared<mariadb_listener_factory>(boost::make_shared<posix_file_logger_factory>(io, "./")));
-		io.run();
-	} catch (std::exception &e) {
-		std::cerr << e.what() << std::endl;
+int		main(int arg_count, char *args[]) {
+	int		status = 0;
+
+	if (arg_count != 2) {
+		std::cout << "Usage: " << args[0] << " <mariadb_host>" << std::endl;
+		status = 1;
+	} else {
+		try {
+			boost::asio::io_context	io;
+			tcp_proxy	proxy(io, args[1], "3306", tcp::endpoint(tcp::v4(), 3306), boost::make_shared<mariadb_listener_factory>(boost::make_shared<posix_file_logger_factory>(io, "./")));
+			io.run();
+		} catch (std::exception &e) {
+			std::cerr << e.what() << std::endl;
+			status = 1;
+		}
 	}
+	return (status);
 }
